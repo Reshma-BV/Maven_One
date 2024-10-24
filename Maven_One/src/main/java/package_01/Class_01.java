4bb0c4f9cb1c4c58ce8ebc2543c4158d85497670
Reshmabv <Reshmabv@Reshma.home>
2024-10-25 01:04:15 +0530

    Maven_one class_01commit

---
+++ Maven_One/src/main/java/package_01/Class_01.java
@@ -0,0 +1,19 @@
+package package_01;
+
+public class Class_01
+{
+	
+	public static void main(String[] args) 
+	{
+		String s= "  welcome  ";
+		String s1="Selenium";
+		String s2="Java";
+		System.out.println(s1.concat(s2));
+		s.length();
+		System.out.println(s);
+		System.out.println(s.trim().length());
+
+
+	}
+
+}
